9302535d9bea34407ba4a197c226f4a3f1bc5ae2
wuqiuli <wutongte@163.com>
2023-03-19 20:05:31 +0800

    feat: add eslint prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+  // 对象的 key 仅在必要时用引号
+  quoteProps: "as-needed",
+};
