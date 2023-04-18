b0b87c78f874a95e1461d4f343a0a61cb1d755c3
wuqiuli <wutongte@163.com>
2023-04-18 21:55:56 +0800

    feat(Notification): add base

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,9 @@
+import react, { FC } from 'react';
+
+interface NotificationProps {}
+
+const Notification: FC<NotificationProps> = (props) => {
+  return <div>2</div>;
+};
+
+export default Notification;
