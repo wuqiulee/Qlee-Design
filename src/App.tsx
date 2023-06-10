a2309b10a34edf0d169d08853dde2832f5ee7dab
wuqiuli <wutongte@163.com>
2023-06-10 19:31:33 +0800

    feat: 删除冗余代码

--- src/App.tsx
+++ src/App.tsx
@@ -11,66 +11,13 @@ library.add(faMoon, faRocket);
 
 function App() {
   return (
-    // <div style={{ margin: 50 }}>
-    //   <Button btnType="primary" theme="light" style={{ color: 'purple' }} block>
-    //     hello
-    //   </Button>
-    //   <Button disabled onClick={() => alert(1)}>
-    //     hello
-    //   </Button>
-    //   <Button btnType="secondary" theme="light">
-    //     hello
-    //   </Button>
-    //   <Button btnType="tertiary" theme="light">
-    //     hello
-    //   </Button>
-    //   <Button btnType="warning" theme="light">
-    //     hello
-    //   </Button>
-    //   <hr />
-    //   <Button size="default">hello</Button>
-    //   <Button btnType="danger" theme="solid">
-    //     hello1313
-    //   </Button>
-    //   <Button btnType="secondary" theme="borderless">
-    //     hello
-    //   </Button>
-    //   <Button btnType="tertiary" theme="solid" disabled>
-    //     hello
-    //   </Button>
-    //   <Button btnType="warning" theme="solid" disabled>
-    //     hello
-    //   </Button>
-    // </div>
-
-    // <div style={{ margin: 50 }}>
-    //   <Banner
-    //     type="info"
-    //     description={<div style={{ color: 'red' }}>banner</div>}
-    //     title={<div>"adad"</div>}
-    //   />
-    //   <Banner type="danger" description="A pre-released version is available." />
-    //   <Banner type="success" description="A pre-released version is available." />
-    //   <Banner
-    //     type="warning"
-    //     description="A pre-released version is available."
-    //     fullMode
-    //     bordered
-    //     title="我是标题"
-    //     style={{ color: 'blue' }}
-    //     onClick={(e) => {
-    //       console.log(e);
-    //       alert(22);
-    //     }}
-    //   />
-    // </div>
     <div style={{ margin: 30 }}>
       <Tabs
         defaultActiveKey="2"
         type="card"
         mode="vertical"
         tabBarExtraContent={<Icon icon="moon" theme="danger" />}
-        onTabClose={(key) => alert(key)}
+        // onTabClose={(key) => alert(key)}
       >
         <TabPane tab="tab1" itemKey="1" disabled>
           <div style={{ color: 'red' }}>child1</div>
