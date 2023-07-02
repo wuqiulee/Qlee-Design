1effc5edbc52d86dec2740ec49886e86350d303e
wuqiuli <wutongte@163.com>
2023-07-02 21:27:05 +0800

    feat: add ci

--- dist/hooks/useClickOutside.d.ts
+++
@@ -1,7 +0,0 @@
-import { RefObject } from 'react';
-declare function useClickOutside(
-  ref: RefObject<HTMLElement>,
-  handler: Function,
-  addClick?: Boolean
-): void;
-export default useClickOutside;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as Banner } from './components/Banner';
-export { default as AutoComplete } from './components/AutoComplete';
-export { default as Icon } from './components/Icon';
-export { default as Input } from './components/Input';
-export { default as Modal } from './components/Modal';
-export { TabPane, Tabs } from './components/Tabs';
-export { default as Tag } from './components/Tag';
